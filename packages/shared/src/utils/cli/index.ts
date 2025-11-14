da71a3850670147b7c7f3e531ee97e376c762c1a
Njabulo Majozi <services@njabulomajozi.com>
2025-11-14 06:29:54 +0200

    feat(shared): add CLI utilities to shared package
    - Move all CLI utilities to packages/shared/src/utils/cli/
    - Move concurrency utilities to packages/shared/src/utils/concurrency/
    - Organize utilities by domain for better maintainability
    - Use namespace export pattern

---
+++ packages/shared/src/utils/cli/index.ts
@@ -0,0 +1,10 @@
+export * as Logger from "./logger";
+export * as Parser from "./parser";
+export * as Config from "./config";
+export * as Confirmation from "./confirmation";
+export * as RemoverBase from "./remover-base";
+export * as PluginLoader from "./plugin-loader";
+export * as Registry from "./registry";
+export * as Graph from "./graph";
+export * as Base from "./base";
+
